c62b0f2f9b033dda01d7716cf120273c17da6b22
Shokat <shokatsaifi@gmail.com>
2014-02-23 17:03:49 +0530

    Update Main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Shokat
  */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
